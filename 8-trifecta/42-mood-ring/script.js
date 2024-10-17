52ba8d24c82fa341c1ee3a36c1d16c28d3e9d02c
AbdelMoncif <65245876+AbdelMoncif@users.noreply.github.com>
2024-10-17 12:30:20 +0100

    Update script.js
    replaced the if statements for number checking with a dictionary lookup containing the random number possibilities and the corresponding color to each number ,

--- 8-trifecta/42-mood-ring/script.js
+++ 8-trifecta/42-mood-ring/script.js
@@ -1,26 +1,11 @@
 // Mood Ring 💍
 // Codédex
 
-const stone = document.getElementById('stone');
 
-const randomNumber = Math.floor(Math.random() * 10) + 1;
 
-if (randomNumber === 1) {
-  stone.style.backgroundColor = "red";
-} else if (randomNumber === 2) {
-  stone.style.backgroundColor = "orange";
-} else if (randomNumber === 3) {
-  stone.style.backgroundColor = "yellow";
-} else if (randomNumber === 4) {
-  stone.style.backgroundColor = "green";
-} else if (randomNumber === 5) {
-  stone.style.backgroundColor = "blue";
-} else if (randomNumber === 6) {
-  stone.style.backgroundColor = "#4169e1";
-} else if (randomNumber === 7) {
-  stone.style.backgroundColor = "#00008b";
-} else if (randomNumber === 8) {
-  stone.style.backgroundColor = "purple";
-} else {
-  stone.style.backgroundColor = "black";
-}
+let stone = document.getElementById("stone");
+let randomNumber = Math.floor(Math.random() * 10);
+const colorDict = {1:"red",2:"orange",3:"yellow",4:"green",5:"blue",6:"#4169e1",7:"#00008b", 8:"purple", 9:"black"};
+
+
+stone.style.backgroundColor = colorDict[randomNumber];
